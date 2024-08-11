379b39f0e42ae5a063b62c6fd40cb43ffdb5ef35
Mintype <96578565+Mintype@users.noreply.github.com>
2024-08-10 23:25:57 -0400

    program done!

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 import time
 import pygetwindow as gw
 import subprocess
+import pyautogui
+import sys
 
 def reopen_spotify():
     # Reopen Spotify
     subprocess.Popen(['spotify'])
+
+def press_play():
+    # Press the play button to unpause music
+    pyautogui.press('playpause')
     
-print("Program started: Have fun listening!")
+spotify_logo = '''
+                  ██████████                  
+             ████████████████████             
+          ██████████████████████████          
+        ██████████████████████████████        
+      ██████████████████████████████████      
+     ████████████████████████████████████     
+   ████████████████████████████████████████   
+  ██████████            ████████████████████  
+  ██████                         ███████████  
+ ███████      ████████               ████████ 
+ ██████████████████████████████        ██████ 
+ ██████████████       ██████████████   ██████ 
+ ████████                     ███████████████ 
+ █████████  █████████████         ███████████ 
+ ██████████████████████████████     █████████ 
+ ███████████            █████████████████████ 
+  █████████                   ██████████████  
+  ██████████████████████████     ███████████  
+   ████████████████████████████████████████   
+     ████████████████████████████████████     
+      ██████████████████████████████████      
+        ██████████████████████████████        
+          ██████████████████████████          
+             ████████████████████             
+                  ██████████                  
+'''
+sys.stdout.buffer.write(spotify_logo.encode('utf-8'))
+print("\nAnti-Spotify-Ads program started: Have fun listening!")
 
 while True:
     windows = gw.getWindowsWithTitle('Advertisement')
@@ -23,6 +57,13 @@ while True:
         # Reopen Spotify
         reopen_spotify()
         print("Reopening Spotify")
+        
+        # Wait for Spotify to fully reopen
+        time.sleep(5)
+        
+        # Press play to unpause music
+        press_play()
+        print("Pressing play to unpause music")
     
     # Wait for 1 second before checking again
     time.sleep(1)
